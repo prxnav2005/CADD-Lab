33d4b7677f47a54f43b04c571a424d32ee41def8
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-08 23:14:17 +0530

    Full adder done

---
+++ Combinational-Circuits/Full-Adder-DataFlowStyle/design.sv
@@ -0,0 +1,9 @@
+module full_adder_d (
+    input a,b,cin,
+    output sum,carry
+);
+
+assign sum = a ^ b ^ cin;
+assign carry = (a & b) | (b & cin)  | (cin & a) ;
+
+endmodule
---
+++ Combinational-Circuits/Full-Adder-DataFlowStyle/tb.sv
@@ -0,0 +1,17 @@
+module tb_full_adder_d;
+    reg a, b, cin;
+    wire sum, carry;
+
+    full_adder_d DUT (a, b, cin, sum, carry);
+
+    initial begin
+        a = 0; b = 0; cin = 0; #10;
+        a = 0; b = 0; cin = 1; #10;
+        a = 0; b = 1; cin = 0; #10;
+        a = 0; b = 1; cin = 1; #10;
+        a = 1; b = 0; cin = 0; #10;
+        a = 1; b = 0; cin = 1; #10;
+        a = 1; b = 1; cin = 0; #10;
+        a = 1; b = 1; cin = 1; #10;
+    end
+endmodule
