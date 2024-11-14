e0efb5641600fed76788e8f3d9ee6d5aec7739e2
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-14 22:50:36 +0530

    changed mux 2 level

--- Combinational-Circuits/4:1-Mux-Using-2-Level-Logic/design.sv
+++ Combinational-Circuits/4:1-Mux-Using-2-Level-Logic/design.sv
@@ -1,8 +1,3 @@
-module mux4_2level(input logic [3:0] d0, d1, d2, d3, input logic [1:0] s, output logic [3:0] y);
-  logic [3:0] s0, s1;
-  
-  assign s0 = (s[0]) ? d1 : d0;
-  assign s1 = (s[0]) ? d3 : d2;
-  
-  assign y = (s[1]) ? s1 : s0;
+module mux4_1(input logic d0, d1, d2, d3, input logic [1:0] s, output logic y);
+    assign y = (~s[1] & ~s[0] & d0) | (~s[1] & s[0] & d1) | (s[1] & ~s[0] & d2) | (s[1] & s[0] & d3);
 endmodule
--- Combinational-Circuits/4:1-Mux-Using-2-Level-Logic/tb.sv
+++ Combinational-Circuits/4:1-Mux-Using-2-Level-Logic/tb.sv
@@ -1,15 +1,14 @@
-module testbench;
-  reg [3:0] d0, d1, d2, d3;
-  reg [1:0] s;
-  wire [3:0] y;
-  
-  mux4_2level DUT (d0, d1, d2, d3, s, y);
+module mux4_1_tb;
+    logic d0, d1, d2, d3;
+    logic [1:0] s;
+    logic y;
 
-  initial begin
-    d0 = 4'b0001; d1 = 4'b0010; d2 = 4'b0100; d3 = 4'b1000;
-    s = 2'b00; #10;
-    s = 2'b01; #10;
-    s = 2'b10; #10;
-    s = 2'b11; #10;
-  end
+    mux4_1 dut (d0, d1, d2, d3, s, y);
+
+    initial begin
+        d0 = 0; d1 = 1; d2 = 0; d3 = 1; s = 2'b00; #10;
+        d0 = 0; d1 = 1; d2 = 0; d3 = 1; s = 2'b01; #10;
+        d0 = 0; d1 = 1; d2 = 0; d3 = 1; s = 2'b10; #10;
+        d0 = 0; d1 = 1; d2 = 0; d3 = 1; s = 2'b11; #10;
+    end
 endmodule
