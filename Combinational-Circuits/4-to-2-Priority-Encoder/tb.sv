0cc7ee8d02ef2e9fa9f1f22aecf76a4c1b4a9a53
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-15 19:14:05 +0530

    priority encoder edited

--- Combinational-Circuits/4-to-2-Priority-Encoder/tb.sv
+++ Combinational-Circuits/4-to-2-Priority-Encoder/tb.sv
@@ -1,18 +1,15 @@
-module testbench;
-  reg [3:0] A;
-  wire [1:0] Y;
-  wire valid;
-  
-  prio_enco DUT (A, Y, valid);
+module tb;
+  logic [3:0] A;
+  logic [1:0] Y;
+
+  prio_enco dut(A, Y);
 
   initial begin
-    A = 4'b0000; #10;
-    A = 4'b0001; #10;
-    A = 4'b0010; #10;
-    A = 4'b0100; #10;
-    A = 4'b1000; #10;
-    A = 4'b1100; #10;
-    A = 4'b1110; #10;
-    A = 4'b1111; #10;
+    A = 4'b0001;
+    #10 A = 4'b0010;
+    #10 A = 4'b0100;
+    #10 A = 4'b1000;
+    #10 A = 4'b0000;
+    #10 $stop;
   end
 endmodule
