9468ec0a2a326a1e0f2ea0dfdc727c10e0022a37
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-29 22:42:30 +0530

    added clock divider

--- Sequential-Circuits/Asynchronous-Counter/design.sv
+++ Sequential-Circuits/Asynchronous-Counter/design.sv
@@ -1,9 +1,18 @@
-module async_counter (
+module async_counter_with_divider (
     input logic clk,
     input logic reset,
     output logic [3:0] q
 );
+    logic [26:0] sclk = 25'b0;
+
     always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            sclk <= 25'b0;
+        else
+            sclk <= sclk + 1;
+    end
+
+    always_ff @(posedge sclk[25] or posedge reset) begin
         if (reset)
             q <= 4'b0000;
         else
